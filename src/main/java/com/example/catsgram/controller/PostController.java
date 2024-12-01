79770dfbe72d8fb023a7fd82f004a176422d93a7
Omirkhon <am.turgimbaev@gmail.com>
2024-12-01 11:35:40 +0500

    log

--- src/main/java/com/example/catsgram/controller/PostController.java
+++ src/main/java/com/example/catsgram/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.example.catsgram.controller;
 
 import com.example.catsgram.model.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,10 +11,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+    private static final Logger log = LoggerFactory.getLogger(PostController.class);
     private final List<Post> posts = new ArrayList<>();
 
     @GetMapping
     public List<Post> findAll() {
+        log.debug("Текущее количество постов: " + posts.size());
         return posts;
     }
 
--- src/main/java/com/example/catsgram/controller/SimpleController.java
+++ src/main/java/com/example/catsgram/controller/SimpleController.java
@@ -1,12 +1,16 @@
 package com.example.catsgram.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
+    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
     @GetMapping("/hello")
     public String helloPage() {
+        log.info("Получен запрос.");
         return "Hello world";
     }
 }
